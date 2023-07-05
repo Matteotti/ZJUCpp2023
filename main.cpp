2ff143f8c63db24ea4d67fac1be3fd3a2bfd7492
Matteotti <mr.matteotti@outlook.com>
2023-07-05 14:18:47 +0800

    [TEST] Red Image Test

--- main.cpp
+++ main.cpp
@@ -8,14 +8,19 @@ int main()
     InitWindow(800, 600, "Hello World");
     SetTargetFPS(60);
 
+    Image red = GenImageColor(256, 256, RED);
+    Texture2D texture = LoadTextureFromImage(red);
+    UnloadImage(red);
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello World", 190, 200, 20, LIGHTGRAY);
+        DrawTexture(texture, 100, 0, WHITE);
         EndDrawing();
     }
 
+    UnloadTexture(texture);
     CloseWindow();
     return 0;
 }
