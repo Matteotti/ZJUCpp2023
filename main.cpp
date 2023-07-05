3fe0e6798084dabac89df7c47a6cc27654c5754e
Matteotti <mr.matteotti@outlook.com>
2023-07-05 10:38:46 +0800

    [TEST] HelloWorld Window Test

--- main.cpp
+++ main.cpp
@@ -5,5 +5,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    InitWindow(800, 600, "Hello World");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello World", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
