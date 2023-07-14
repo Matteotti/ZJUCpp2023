3fbac95aecdee5d728852b8d60fbc98015b88783
Matteotti <mr.matteotti@outlook.com>
2023-07-15 00:38:21 +0800

    [NEW] Basic Move And Animation

--- src/view/GameView.h
+++ src/view/GameView.h
@@ -15,41 +15,51 @@
 #include "../../includes/animation.h"
 #include "../../includes/player.h"
 
-
-class Button{
+class Button
+{
 private:
     raylib::Image image;
     raylib::Rectangle src;
     bool isHovered;
     std::function<void()> command;
+
 public:
-    Button(std::string path, raylib::Rectangle src, std::function<void()> command){
+    Button(std::string path, raylib::Rectangle src, std::function<void()> command)
+    {
         image.Load(path);
         this->src = src;
         this->isHovered = false;
         this->command = command;
     }
-    void UpdateButton(){
-        if (CheckCollisionPointRec(GetMousePosition(), src)){
+    void UpdateButton()
+    {
+        if (CheckCollisionPointRec(GetMousePosition(), src))
+        {
             isHovered = true;
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            {
                 command();
             }
-        }else{
+        }
+        else
+        {
             isHovered = false;
         }
     }
-    void DrawButton(){
-        if(isHovered){
+    void DrawButton()
+    {
+        if (isHovered)
+        {
             raylib::Texture texture;
             texture.Load(image);
-            texture.Draw(src,src.GetPosition(),WHITE);
+            texture.Draw(src, src.GetPosition(), WHITE);
             texture.Unload();
         }
     }
 
-    //overload = operator
-    Button& operator=(const Button& button){
+    // overload = operator
+    Button &operator=(const Button &button)
+    {
         this->image = button.image;
         this->src = button.src;
         this->isHovered = button.isHovered;
@@ -57,41 +67,52 @@ public:
         return *this;
     }
 
-    ~Button(){
+    ~Button()
+    {
         image.Unload();
     }
 };
 
-class Menu{
+class Menu
+{
 private:
     std::vector<Button> buttons;
     raylib::Texture background;
+
 public:
-    Menu(){}
-    Menu(std::vector<Button> buttons, std::string background_path){
+    Menu() {}
+    Menu(std::vector<Button> buttons, std::string background_path)
+    {
         this->buttons = std::move(buttons);
         background.Load(background_path);
     }
-    void DrawMenu(){
+    void DrawMenu()
+    {
         background.Draw(0, 0);
-        for (auto &button : buttons) {
+        for (auto &button : buttons)
+        {
             button.DrawButton();
         }
     }
-    void UpdateMenu(){
-        for (auto &button: buttons) {
+    void UpdateMenu()
+    {
+        for (auto &button : buttons)
+        {
             button.UpdateButton();
         }
     }
-    void SetButtons(std::vector<Button> buttonsList){
+    void SetButtons(std::vector<Button> buttonsList)
+    {
         this->buttons = std::move(buttonsList);
     }
-    ~Menu(){
+    ~Menu()
+    {
         background.Unload();
     }
 };
 
-class UI{
+class UI
+{
 private:
     raylib::Image HP_bar_unit;
     raylib::Image HP_bar_unit_empty;
@@ -113,9 +134,10 @@ private:
     int MP_count{};
 
 public:
-    UI(){}
+    UI() {}
 
-    UI(const std::string& HP_bar_unit_path, const std::string& MP_bar_path, const std::string& Background_path){
+    UI(const std::string &HP_bar_unit_path, const std::string &MP_bar_path, const std::string &Background_path)
+    {
         HP_bar_unit.Load(HP_bar_unit_path);
         MP_bar.Load(MP_bar_path);
         Background.Load(Background_path);
@@ -123,23 +145,28 @@ public:
         MP_count = 0;
     }
 
-    void DrawUI(){
+    void DrawUI()
+    {
         raylib::Texture tBackground;
         tBackground.Load(Background);
         tBackground.Draw(Background_position.x, Background_position.y);
         tBackground.Unload();
-        for (int i = 0; i < PLAYER_MAX_HP; ++i) {
+        for (int i = 0; i < PLAYER_MAX_HP; ++i)
+        {
             raylib::Texture tHP_bar_unit;
             tHP_bar_unit.Load(HP_bar_unit);
             raylib::Texture tHP_bar_unit_empty;
             tHP_bar_unit_empty.Load(HP_bar_unit_empty);
-            if(i<HP_count) {
+            if (i < HP_count)
+            {
                 tHP_bar_unit.Draw(raylib::Vector2(HP_bar_position.x + i * HP_bar_unit.GetWidth(),
                                                   HP_bar_position.y),
                                   0,
                                   HP_bar_unit_scale,
                                   WHITE);
-            } else {
+            }
+            else
+            {
                 tHP_bar_unit_empty.Draw(raylib::Vector2(HP_bar_position.x + i * HP_bar_unit.GetWidth(),
                                                         HP_bar_position.y),
                                         0,
@@ -154,58 +181,70 @@ public:
         tMP_bar.Draw(raylib::Rectangle(0,
                                        MP_bar.GetHeight() * (PLAYER_MAX_MP - MP_count) / PLAYER_MAX_MP,
                                        MP_bar.GetWidth(),
-                                       MP_bar.GetHeight() * MP_count/PLAYER_MAX_MP),
+                                       MP_bar.GetHeight() * MP_count / PLAYER_MAX_MP),
                      MP_bar_position,
                      MP_count < 33 ? WHITE : GRAY);
         tMP_bar.Unload();
     }
 
-    void UpdateUI(const int HP, const int MP){
+    void UpdateUI(const int HP, const int MP)
+    {
         HP_count = HP;
         MP_count = MP;
     }
 
-    void SetHP_bar_unit(const std::string &path){
+    void SetHP_bar_unit(const std::string &path)
+    {
         HP_bar_unit.Load(path);
     }
 
-    void SetHP_bar_unit_empty(const std::string &path){
+    void SetHP_bar_unit_empty(const std::string &path)
+    {
         HP_bar_unit_empty.Load(path);
     }
 
-    void SetMP_bar(const std::string &path){
+    void SetMP_bar(const std::string &path)
+    {
         MP_bar.Load(path);
     }
 
-    void SetBackground(const std::string &path){
+    void SetBackground(const std::string &path)
+    {
         Background.Load(path);
     }
 
-    void SetHP_bar_unit_position(const raylib::Vector2 &HP_bar_unit_position) {
+    void SetHP_bar_unit_position(const raylib::Vector2 &HP_bar_unit_position)
+    {
         HP_bar_position = HP_bar_unit_position;
     }
 
-    void SetMP_bar_position(const raylib::Vector2 &position) {
+    void SetMP_bar_position(const raylib::Vector2 &position)
+    {
         this->MP_bar_position = position;
     }
 
-    void SetBackground_position(const raylib::Vector2 &position) {
+    void SetBackground_position(const raylib::Vector2 &position)
+    {
         this->Background_position = position;
     }
 
-    void SetHP_bar_unit_scale(float scale) {
+    void SetHP_bar_unit_scale(float scale)
+    {
         this->HP_bar_unit_scale = scale;
     }
 
-    void SetMP_bar_scale(float scale) {
+    void SetMP_bar_scale(float scale)
+    {
         this->MP_bar_scale = scale;
     }
 
-    void SetBackground_scale(float scale) {
+    void SetBackground_scale(float scale)
+    {
         this->Background_scale = scale;
     }
 
-    ~UI(){
+    ~UI()
+    {
         HP_bar_unit.Unload();
         MP_bar.Unload();
         Background.Unload();
@@ -218,51 +257,46 @@ private:
     int HorizontalInput = 0;
     int VerticalInput = 0;
 
-    std::function<void()> updateViewCommand;
-    std::function<void()> increaseScoreCommand;
-    std::function<void(bool)> setGameOverCommand;
-
     std::function<void(direction)> playerMoveCommand;
     std::function<void(bool)> playerJumpCommand;
+    std::function<void()> playerUpdateJumpSpeedCommand;
+    std::function<void()> playerCheckWallCommand;
+    std::function<void()> playerAniamtorUpdateCommand;
+    std::function<void()> playerAniamtionUpdateCommand;
+    std::function<void()> playerUpdatePositionCommand;
+    std::function<void()> playerUpdateAnimationFrameCommand;
+    std::function<void(raylib::Vector2)> playerUpdateAnimationRectCommand;
     std::function<void(direction)> playerAttackCommand;
+    std::function<void()> drawPlayerCommand;
 
     std::shared_ptr<GameCommon> gameCommonPtr;
-    
-
-    std::function<void()> attackTopCommand;
-    std::function<void()> attackDownCommand;
 
 public:
     GameView();
-    void setCommon(std::shared_ptr<GameCommon> gameCommon);
-    //void drawExample(int score, bool gameOver);
-    //void UpdateScore();
-    //void GameOver();
     void UpdatePlayerMove();
     void UpdatePlayerJump();
     void UpdatePlayerAttack(bool isFacingRight);
+    void Update();
 
     void Draw(
-            std::string path,
-            raylib::Vector2 position,
-            raylib::Rectangle src
-    );
-
-    void SetIncreaseScoreCommand(std::function<void()> command);
-    void setSetGameOverCommand(std::function<void(bool)> command);
+        std::string path,
+        raylib::Vector2 position,
+        raylib::Rectangle src);
     void SetPlayerMoveCommand(std::function<void(direction)> command);
     void SetPlayerJumpCommand(std::function<void(bool)> command);
-    void SetPlayerAttackCommand(std::function<void(bool)> command);
-
-    std::shared_ptr<GameCommon> getGameCommonPtr() {
+    void SetPlayerUpdateJumpSpeedCommand(std::function<void()> command);
+    void SetPlayerCheckWallCommand(std::function<void()> command);
+    void SetPlayerAnimatorUpdateCommand(std::function<void()> command);
+    void SetPlayerAnimationUpdateCommand(std::function<void()> command);
+    void SetPlayerUpdatePositionCommand(std::function<void()> command);
+    void SetPlayerUpdateAnimationFrameCommand(std::function<void()> command);
+    void SetPlayerUpdateAnimationRectCommand(std::function<void(raylib::Vector2)> command);
+    void SetPlayerAttackCommand(std::function<void(direction)> command);
+    void SetDrawPlayerCommand(std::function<void()> command);
+    std::shared_ptr<GameCommon> getGameCommonPtr()
+    {
         return gameCommonPtr;
     }
-    
-
-    void Attack();
-    void setAttackTopCommand(std::function<void()> command);
-    void setAttackDownCommand(std::function<void()> command);
-
 };
 
 #endif // CPPGAMEJAM_GAMEVIEW_H
